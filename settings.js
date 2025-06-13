e91517501ac6e24f8bbe8b85726bb18e74df150f
queenruhi500 <queenruhi500@gmail.com>
2025-06-13 20:42:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,6 +21,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",// session id 💙
+SESSION_ID: process.env.SESSION_ID || "𝚁𝙰𝚅𝙸𝙽𝙳𝚄-𝙼𝙳=D1ABTSyC#hyQLz8OAUqtYy6x39k8Mqa1shQC13tIJTX683SPZACo",// session id 💙
 HACKER : process.env.HACKER || "94704638406" //Addmin number 💙
 };
